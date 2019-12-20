829a6b755a0c99018c75204871bb28a5cc6ce41f
KandasamyMurugan <kandu069@gmail.com>
2019-12-20 13:23:10 +0000

    Comments and Variables examples

---
+++ Comments-Variables/Comments-Variables.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Comments always start with # symbol or Next to the content
+
+echo "Hello World" # This is hello world program
+
+##### Variables
+
+# There are two different kind of Variables
+
+# 1) System Variables are already defined and handling by Operating system
+echo $PATH
+
+echo $HOME
+
+echo $PWD
+
+echo $HOME
+
+# 2) User defined variables are defining and managing ny user.
+
+name=Kanda
+
+echo "User name is $name"
+
+AGE=29
+
+echo "User age is $AGE"
+
+
+10VAL=100 #Variables should not be starts with number
+
+echo "Value $10VAL"
