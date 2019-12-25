b6c3546610c0d77b2546a93866f91fdf871fd55e
root <root@kmurugan2c.mylabserver.com>
2019-12-25 15:21:02 +0000

    all

--- stack.sh
+++ stack.sh
@@ -8,7 +8,16 @@ USER_ID=`id -u`
 
 ################################################################################################################################
 
-if [ $USER_ID -ne 1000 ]; then
+if [ $USER_ID -ne 0 ]; then
 	echo -e "\e[31mYou should be a root user to execute this script\e[0m"
 exit 1
 fi
+
+yum install httpd -y
+
+curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/index.html -o /var/www/html/index.html
+systemctl enable httpd 
+systemctl start httpd
+
+touch /etc/httpd/conf.d/app-proxy.conf
+
